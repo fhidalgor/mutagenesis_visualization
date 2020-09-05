24f6830e85c6b09265755de9fe21be70a7c59ab1
Frank <fx@FXs-MacBook-Pro.local>
2020-09-05 14:06:25 -0700

    test38

--- docs/conf.py
+++ docs/conf.py
@@ -31,8 +31,7 @@ needs_sphinx = '1.7.3'
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom ones.
 extensions = ['sphinx.ext.autodoc', 'sphinx.ext.coverage', 'sphinx.ext.napoleon', 'numpydoc',
-'sphinx.ext.autosectionlabel', 'sphinx.ext.intersphinx', 'sphinx.ext.mathjax', 'IPython.sphinxext.ipython_console_highlighting',
-'IPython.sphinxext.ipython_directive']
+'sphinx.ext.autosectionlabel', 'sphinx.ext.intersphinx', 'sphinx.ext.mathjax']
 
 # Allow errors nbsphinx
 nbsphinx_allow_errors = True
@@ -42,7 +41,7 @@ nbsphinx_execute = 'never'
 
 # mock import modules
 autodoc_mock_imports = ['Bio', 'ipymol', 'Shannon', 'logomaker', 'collections', 'adjustText', 'seaborn',
-'scipy', 'sklearn', 'pandas', 'Import_notebook', 'mutagenesis_visualization', 'numpy']
+'scipy', 'sklearn', 'pandas', 'Import_notebook', 'mutagenesis_visualization', 'numpy', 'matplotlib']
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
