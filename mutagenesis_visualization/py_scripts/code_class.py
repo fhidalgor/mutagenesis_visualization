c2dd7a198d224c4f6671ff1471758bd722d3477b
Frank <31396084+fhidalgor@users.noreply.github.com>
2020-11-17 11:33:09 -0800

    added some tests

---
+++ mutagenesis_visualization/py_scripts/code_class.py
@@ -0,0 +1,204 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Import Modules
+
+# In[ ]:
+
+
+# Regular libraries
+import numpy as np
+import itertools
+
+# Package libraries
+try:
+    import import_notebook
+except ModuleNotFoundError:
+    pass
+
+from code_kernel import (plot_kernel, plot_hist)
+from code_heatmaps import (
+    plot_heatmap, plot_heatmap_rows, plot_heatmap_columns)
+from code_bar import (plot_mean, plot_meandifferential,
+                      plot_position, plot_meancounts, plot_library_representation)
+from code_scatter import (plot_scatter)
+from code_PCA import (
+    plot_correlation, plot_individual_correlation, plot_group_correlation, plot_pca)
+from code_other import (plot_rank, plot_miniheatmap,
+                        plot_neighboreffect, plot_secondary, plot_roc, plot_cumulative)
+from code_3D import (plot_scatter_3D, plot_scatter_3D_pdbprop, plot_pymol)
+from code_plotly import (plot_rank_plotly, plot_scatter_plotly)
+from code_utils import (_is_DNA, _translate_codons, _transform_sequence,
+                        _transform_dataset, _select_SNV, _select_nonSNV, _transform_secondary)
+
+
+# # Define Classes
+
+# In[ ]:
+
+
+class Counts:
+    '''
+    *Counts* represents the output of reading a fastq file.
+
+    Parameters
+    ----------
+    df : pandas dataframe
+        Dataframe containing the counts per codon.
+
+    start_position : int, default None
+        First position in the protein sequence that will be used for the first column of the
+        array. If a protein has been mutated only from residue 100-150, then if start_position = 100,
+        the algorithm will trim the first 99 amino acids in the input sequence. The last 
+        residue will be calculated based on the length of the input array. We have set the default value to 2
+        because normally the Methionine in position 1 is not mutated.
+
+    aminoacids : list, default None
+        List of aminoacids (in order). Stop codon needs to be '*'.
+        If none, it will use the index of the dataframe   
+
+    '''
+
+    def __init__(self, df, start_position=None, aminoacids=None):
+        self.dataframe = df
+
+        if start_position:
+            self.start_position = start_position
+            self.positions = np.arange(start_position, len(df.columns)+1)
+        else:  # if none, use the columns of the dataframe
+            self.positions = list(df.columns)
+            self.start_position = self.positions[0]
+
+        if aminoacids:
+            self.aminoacids = aminoacids
+        else:  # if aminoacids is none, use the index of the dataframe
+            if _is_DNA(df):
+                self.aminoacids = _translate_codons(df)
+            else:
+                self.aminoacids = list(df.index)
+        # bar plots
+    mean_counts = plot_meancounts
+    library_representation = plot_library_representation
+
+
+# In[ ]:
+
+
+class Screen:
+    '''
+    *Screen* represents a mutagenesis experiment. If you are doing deep scan 
+    mutagenesis, then every amino acid in the protein has been mutated to every possible 
+    amino acid. For example, if there was a leucine at position 2, then this leucine would
+    be mutated to the other 19 naturally occurring amino acids. However, you can also use the 
+    package if you only have a handful of amino acid substitutions.
+
+
+    Parameters
+    -----------
+    dataset : array
+        2D matrix containing the enrichment scores of the point mutants. Columns will contain the
+        amino acid substitutions, rows will contain the enrichment for each residue in the protein sequence.
+
+    sequence : str
+        Protein sequence (columns) in 1 letter code format.
+
+    aminoacids : list, default list('ACDEFGHIKLMNPQRSTVWY*')
+        Amino acid substitutions (rows). Submit in the same order that is used for the array.
+
+    start_position : int, default 2
+        First position in the protein sequence that will be used for the first column of the
+        array. If a protein has been mutated only from residue 100-150, then if start_position = 100,
+        the algorithm will trim the first 99 amino acids in the input sequence. The last 
+        residue will be calculated based on the length of the input array. We have set the default value to 2
+        because normally the Methionine in position 1 is not mutated.
+
+    secondary : list, optional
+        This parameter is used to group the data by secondary structure. The format is 
+        the name of the secondary structure multiplied by the residue length of that motif.
+        Example : [['β1']*(8),['L1']*(7),['α1']*(9),...,].
+
+    roc_df: Pandas dataframe, optional
+        A dataframe that contains a column of variants labeled 'Variant' with a column labeled 'Class'
+        containing the true class of that mutation. This can be used to compare enrichment scores to some label (such as 
+        pathogenicity as found in a Cancer database) using ROC AUC.
+
+    fillna : int, default 0
+        How to replace NaN values.
+
+
+    Attributes
+    ------------
+    dataframe : pandas dataframe
+        Contains the enrichment scores, position, sequence.
+
+    Other attributes are same as input parameters: dataset, aminoacids, start_position, roc_df, secondary
+
+    '''
+
+    def __init__(self, dataset, sequence, aminoacids=list('ACDEFGHIKLMNPQRSTVWY*'),
+                 start_position=2, fillna=0, secondary=None, roc_df=None):
+        self.dataset = np.array(dataset)
+        self.aminoacids = aminoacids
+        self.start_position = start_position
+        self.end_position = len(self.dataset[0])+start_position
+        self.sequence_raw = ''.join(sequence)  # why I am doing this?
+        self.sequence = _transform_sequence(
+            self.dataset, self.sequence_raw, self.start_position)
+        self.dataframe_stopcodons, self.dataframe = _transform_dataset(
+            self.dataset, self.sequence, self.aminoacids, self.start_position, fillna)
+        self.dataframe_SNV = _select_SNV(self.dataframe)
+        self.dataframe_nonSNV = _select_nonSNV(self.dataframe)
+
+        # Optional parameters
+        self.roc_df = roc_df
+        self.secondary = secondary
+        if self.secondary is not None:
+            self.secondary, self.secondary_dup = _transform_secondary(
+                self.dataset, self.secondary, self.start_position, self.aminoacids)
+
+        # Assert messages
+        assert len(sequence) >= len(
+            self.dataset[0]), 'Input sequence is not long enough'
+
+    # Methods (Associated functions)
+
+    # code_kernel
+    kernel = plot_kernel
+    histogram = plot_hist
+
+    # code_heatmaps
+    heatmap = plot_heatmap
+    heatmap_rows = plot_heatmap_rows
+    heatmap_columns = plot_heatmap_columns
+
+    # code_bar
+    mean = plot_mean
+    differential = plot_meandifferential
+    position = plot_position
+
+    # code_scatter
+    scatter = plot_scatter
+
+    # code_PCA
+    correlation = plot_correlation
+    individual_correlation = plot_individual_correlation
+    group_correlation = plot_group_correlation
+    pca = plot_pca
+
+    # code_Other
+    rank = plot_rank
+    miniheatmap = plot_miniheatmap
+    neighboreffect = plot_neighboreffect
+    secondary_mean = plot_secondary
+    roc = plot_roc
+    cumulative = plot_cumulative
+
+    # code_3D
+    scatter_3D = plot_scatter_3D
+    scatter_3D_pdbprop = plot_scatter_3D_pdbprop
+    pymol = plot_pymol
+
+    # code_plotly
+    rank_plotly = plot_rank_plotly
+    scatter_plotly = plot_scatter_plotly
+
---
+++ mutagenesis_visualization/py_scripts/code_demo.py
@@ -0,0 +1,221 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Import modules
+
+# In[1]:
+
+
+# Regular libraries
+import numpy as np
+import pandas as pd
+import itertools
+from os import path
+import os
+
+# Package libraries
+try:
+    import import_notebook
+except ModuleNotFoundError:
+    pass
+import code_class
+
+
+# # Demo
+
+# In[ ]:
+
+
+def demo(figure='heatmap'):
+    """
+    Performs a demonstration of the mutagenesis_visualization software.
+
+    Parameters
+    -----------
+    figure : str, default 'heatmap'
+        There are 5 example plots that can be displayed to test the package is working on your station.
+        The 5 options are 'heatmap', 'miniheatmap', 'mean', 'kernel' and 'pca'. Check the documentation for more information.
+
+    Returns
+    -------
+    None.
+    """
+    # Use relative file import to access the data folder
+    location = os.path.dirname(os.path.realpath(__file__))
+    my_file = os.path.join(location, 'data', 'HRas166_RBD.csv')
+
+    # Load enrichment scores
+    hras_enrichment_RBD = np.genfromtxt(my_file, delimiter=',')
+
+    # Define protein sequence
+    hras_sequence = 'MTEYKLVVVGAGGVGKSALTIQLIQNHFVDEYDPTIEDSYRKQVVIDGETCLLDILDTAGQEEYSAMRDQYMRTGEGFLCVFAINNTKSFEDIHQYREQIKRVKDSDDVPMVLVGNKCDLAARTVESRQAQDLARSYGIPYIETSAKTRQGVEDAFYTLVREIRQHKLRKLNPPDESGPG'
+
+    # Define secondary structure
+    secondary = [['L0'], ['β1']*(9-1), ['L1']*(15-9), ['α1']*(25-15), ['L2']*(36-25), ['β2']*(46-36), ['L3']*(48-46),
+                 ['β3']*(58-48), ['L4'] * (64-58), ['α2'] *
+                 (74-64), ['L5']*(76-74), ['β4']*(83-76),
+                 ['L6']*(86-83), ['α3']*(103-86), ['L7']*(110-103), ['β5'] *
+                 (116-110), ['L8']*(126-116), ['α4']*(137-126),
+                 ['L9']*(140-137), ['β6']*(143-140), ['L10']*(151-143), ['α5']*(172-151), ['L11']*(190-172)]
+
+    # Create object
+    hras_RBD = code_class.Screen(dataset=hras_enrichment_RBD,
+                                 sequence=hras_sequence, secondary=secondary)
+
+    if figure == 'heatmap':
+        # Create heatmap plot
+        hras_RBD.heatmap(title='H-Ras 2-166', show_cartoon=True)
+    elif figure == 'miniheatmap':
+        # Condensed heatmap
+        hras_RBD.miniheatmap(title='Wt residue H-Ras')
+    elif figure == 'mean':
+        # Mean enrichment by position
+        hras_RBD.mean(figsize=[6, 2.5], mode='mean',
+                      show_cartoon=True, yscale=[-2, 0.5], title='')
+    elif figure == 'kernel':
+        # Plot kernel dist using sns.distplot.
+        hras_RBD.kernel(histogram=True, title='H-Ras 2-166', xscale=[-2, 1])
+    elif figure == 'pca':
+        # PCA by amino acid substitution
+        hras_RBD.pca(dimensions=[0, 1], figsize=(
+            2, 2), adjustlabels=True, title='')
+    return
+
+
+def demo_datasets():
+    '''
+    Loads example datasets so the user can play with it.
+
+    Parameters
+    -----------
+    None
+
+    Returns
+    --------
+    data_dict : dictionary
+        Dictionary that contains the datasets used to create the plots on the documentation.
+    '''
+
+    # Use relative file import to access the data folder
+    location = os.path.dirname(os.path.realpath(__file__))
+
+    # Create dictionary where to store data
+    data_dict = {}
+
+    # Retrieve H-Ras datasets and store in dict
+    my_file = os.path.join(location, 'data', 'HRas166_RBD.csv')
+    hras_enrichment_RBD = np.genfromtxt(my_file, delimiter=',')
+    data_dict['array_hras_RBD'] = hras_enrichment_RBD
+
+    my_file = os.path.join(location, 'data', 'HRas166_GAPGEF.csv')
+    hras_enrichment_GAPGEF = np.genfromtxt(my_file, delimiter=',')
+    data_dict['array_hras_GAPGEF'] = hras_enrichment_GAPGEF
+
+    # Beta lactamase data
+    my_file = os.path.join(location, 'data', 'df_bla_raw.pkl')
+    df_bla_raw = pd.read_pickle(my_file)
+    data_dict['df_bla'], sequence_bla = parse_pivot(
+        df_bla_raw, col_data='DMS_amp_625_(b)')
+
+    # Sumo
+    my_file = os.path.join(location, 'data', 'df_sumo1_raw.pkl')
+    df_sumo1_raw = pd.read_pickle(my_file)
+    data_dict['df_sumo1'], sequence_sumo1 = parse_pivot(
+        df_sumo1_raw, col_data='DMS')
+
+    # MAPK1
+    my_file = os.path.join(location, 'data', 'df_mapk1_raw.pkl')
+    df_mapk1_raw = pd.read_pickle(my_file)
+    data_dict['df_mapk1'], sequence_mapk1 = parse_pivot(
+        df_mapk1_raw, col_data='DMS_DOX')
+
+    # UBE2I
+    my_file = os.path.join(location, 'data', 'df_ube2i_raw.pkl')
+    df_ube2i_raw = pd.read_pickle(my_file)
+    data_dict['df_ube2i'], sequence_ube2i = parse_pivot(
+        df_ube2i_raw, col_data='DMS')
+
+    # TAT
+    my_file = os.path.join(location, 'data', 'df_tat.pkl')
+    data_dict['df_tat'] = pd.read_pickle(my_file)
+
+    # REV
+    my_file = os.path.join(location, 'data', 'df_rev.pkl')
+    data_dict['df_rev'] = pd.read_pickle(my_file)
+
+    # asynuclein
+    my_file = os.path.join(location, 'data', 'df_asynuclein.pkl')
+    data_dict['df_asynuclein'] = pd.read_pickle(my_file)
+
+    # APH
+    my_file = os.path.join(location, 'data', 'df_aph.pkl')
+    data_dict['df_aph'] = pd.read_pickle(my_file)
+
+    # b11L5
+    my_file = os.path.join(location, 'data', 'df_b11L5F_raw.pkl')
+    df_b11L5F_raw = pd.read_pickle(my_file)
+    data_dict['df_b11L5F'], sequence_b11L5F = parse_pivot(
+        df_b11L5F_raw, col_data='relative_tryp_stability_score')
+
+    return data_dict
+
+
+def demo_pdbs():
+    '''
+    Loads example pdbs so the user can play with it (5p21, 1erm, 1a5r, 1nd4).
+
+    Parameters
+    -----------
+    None
+
+    Returns
+    --------
+    data_dict : dictionary
+        Dictionary that contains the pdbs used to create the plots on the documentation.
+    '''
+
+    # Use relative file import to access the data folder
+    location = os.path.dirname(os.path.realpath(__file__))
+
+    # Create dictionary where to store data
+    pdb_dict = {}
+
+    # H-Ras
+    pdb_dict['5p21'] = os.path.join(location, 'data', '5p21.pdb')
+
+    # Beta lactamase data
+    pdb_dict['1erm'] = os.path.join(location, 'data', '1erm.pdb')
+
+    # Sumo
+    pdb_dict['1a5r'] = os.path.join(location, 'data', '1a5r.pdb')
+
+    # APH
+    pdb_dict['1nd4'] = os.path.join(location, 'data', '1nd4.pdb')
+
+    return pdb_dict
+
+
+def demo_fasta():
+    '''
+    Loads example fasta so the user can play with it.
+
+    Parameters
+    -----------
+    None
+
+    Returns
+    --------
+    data_dict : dictionary
+        Dictionary that contains the fasta used to extract the sequence conservation.
+    '''
+
+    # Use relative file import to access the data folder
+    location = os.path.dirname(os.path.realpath(__file__))
+
+    # Create dictionary where to store data
+    fasta_dict = {}
+    fasta_dict['ras'] = os.path.join(
+        location, 'data', 'Ras_family_trimmed.fasta')
+
+    return fasta_dict
+
