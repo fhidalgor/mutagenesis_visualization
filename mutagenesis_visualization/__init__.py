fedadbb6ca114a3db31b01bb98765b5db3a66311
Frank <31396084+fhidalgor@users.noreply.github.com>
2020-11-19 21:30:45 -0800

    changed directories

--- mutagenesis_visualization/__init__.py
+++ mutagenesis_visualization/__init__.py
@@ -1,8 +1,14 @@
-from .mutagenesis_visualization import *
+'''__all__ = []
+
+
+from .scripts.code_process_data import (count_reads, count_fastq, calculate_enrichment, assemble_avengers, msa_enrichment)
+from .scripts.code_class import Screen
+
+
 
 __author__ = "Frank Hidalgo"
 __version__ = "0.0.1"
 __title__ = "Mutagenesis Visualization"
 __license__ = "GPLv3"
 __author_email__ = "fhidalgoruiz@berkeley.edu"
-
+'''
