b5d0c85a3f4c268c1851b12e60454235323c9930
Frank <31396084+fhidalgor@users.noreply.github.com>
2020-11-20 20:20:42 -0800

    updated tests

--- mutagenesis_visualization/__init__.py
+++ mutagenesis_visualization/__init__.py
@@ -1,8 +1,15 @@
-'''__all__ = []
-
-
-from .scripts.code_process_data import (count_reads, count_fastq, calculate_enrichment, assemble_avengers, msa_enrichment)
-from .scripts.code_class import Screen
+from mutagenesis_visualization.main.scripts.code_process_data import (count_reads, count_fastq, calculate_enrichment, assemble_avengers, msa_enrichment)
+from mutagenesis_visualization.main.scripts.code_class import Screen, Counts
+from mutagenesis_visualization.main.scripts.code_heatmaps import (
+        _hierarchical_sort, _helix, _labels, _sheet, _loop)
+import mutagenesis_visualization.main.scripts.code_kwargs as code_kwargs
+from mutagenesis_visualization.main.scripts.code_process_data import (
+        count_reads
+)
+from mutagenesis_visualization.main.scripts.code_synthesis import (
+        generate_primers, _primerdesign, _create_primers_list, create_variants
+)
+import mutagenesis_visualization.main.scripts.code_utils as code_utils
 
 
 
@@ -11,4 +18,3 @@ __version__ = "0.0.1"
 __title__ = "Mutagenesis Visualization"
 __license__ = "GPLv3"
 __author_email__ = "fhidalgoruiz@berkeley.edu"
-'''
---
+++ mutagenesis_visualization/conftest.py
@@ -0,0 +1,1 @@
+#https://stackoverflow.com/questions/10253826/path-issue-with-pytest-importerror-no-module-named-yadayadayada
