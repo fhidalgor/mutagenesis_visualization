87055cae4dee185b3e4048886373b1a89852c7ea
Frank <frank@pop-os.localdomain>
2021-08-30 14:02:27 -0700

    modified conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -12,10 +12,10 @@
 #
 import sys
 import os
+sys.path.insert(0, os.path.abspath('../'))
 from mutagenesis_visualization import (__author__, __version__,
                                         __title__, __description__)
 
-sys.path.insert(0, os.path.abspath('../../'))
 
 # -- Project information -----------------------------------------------------
 project = __title__
